5b48b24c647f223294980e3cd8b00c0e73d6076e
Joan De La Rosa <joan_delarosa@intellisys.com.do>
2018-05-06 03:06:11 -0400

    git add test_case redirection and subelementadd file variable

--- test_site.py
+++ test_site.py
@@ -1,29 +1,20 @@
 import unittest
 import time
 import page
-import testhelper
-import requests
+import variable as v
 
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains as AC
 from selenium.webdriver.common.keys import Keys
-
-elements = ['INICIO', 'PORTAFOLIO', 'CONÓCENOS', 'EMPLEOS', 'BLOG']
-links = [
-    'http://gbh.com.do/',
-    'http://gbh.com.do/portafolio/',
-    'http://gbh.com.do/soluciones-gbh/',
-    'http://gbh.com.do/empleos/',
-    'http://gbh.com.do/noticias-de-tecnologia/']
 
 
 class mainMenu(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # cls.driver = webdriver.Firefox()
-        cls.driver = webdriver.Chrome('chromedriver')
-        cls.driver.get("http://www.gbh.com.do")
+        cls.driver = webdriver.Firefox()
+        # cls.driver = webdriver.Chrome('chromedriver')
+        cls.driver.get(v.url[0])
         cls.main_page = page.HomePage(cls.driver)
         time.sleep(5)
         # print(cls.driver.title)
@@ -47,43 +38,69 @@ class mainMenu(unittest.TestCase):
         for i in range(0, len(items)):
             if 'menu-item-object-page' in items[i].get_attribute('class'):  # menu-item-object-page
                 menu_elements.append(items[i].text)
-        self.assertEqual(menu_elements, elements, 'The Menu Elements missing and/or has incorrect Elements')
+        self.assertEqual(menu_elements, v.elements, 'The Menu Elements missing and/or has incorrect Elements')
 
     def test_menu_elements_links(self):
         """ Homepage - Menu Elements Links, Verifying The Elements/items links are complete or corret"""
         elements_links = []
         items = self.main_page.get_main_menu_elements()
         for i in range(0, len(items)):
-            if 'menu-item-object-page' in items[i].get_attribute('class'):  # menu-item-object-page
+            if v.html_class[0] in items[i].get_attribute('class'):  # menu-item-object-page
                 elements_links.append(items[i].find_element_by_tag_name('a').get_attribute('href'))
-        self.assertEqual(elements_links, links, 'The Menu Links missing and/or has incorrect Links')
+        self.assertEqual(elements_links, v.links, 'The Menu Links missing and/or has incorrect Links')
 
     def test_menu_elements_redirect(self):
         """ Homepage - Menu Element, Verifying The Redirection and if is Clickable"""
-        menu_elements = []
-        driver = self.main_page
-        items = driver.get_main_menu_elements()
+        driver = self.driver
+        items = self.main_page.get_main_menu_elements()
         for i in range(0, len(items)):
-            if 'menu-item-object-page' in items[i].get_attribute('class'):  # menu-item-object-page
+            if v.html_class[0] in items[i].get_attribute('class'):
+                element = items[i].find_element_by_tag_name('a')
+
                 try:
-                    element = driver.find_element_by_partial_link_text(items[i].text)
-                    handles = driver.window_handles
-                    # menu_elements = testhelper.click_and_open_tab(element, dr, menu_elements, handles,)
-                    AC(driver).key_down(Keys.COMMAND).click(element).key_up(Keys.COMMAND).perform()
-                    time.sleep(2)
-                    driver.switch_to.window(handles[1])
-                    time.sleep(2)
-                    # r = requests.get(driver.current_url)
-                    # menu_elements.append(driver.current_url, r.status_code)
-                    driver.close()
-                    driver.switch_to.window(handles[0])
+                    old_url = self.driver.current_url
+                    main_window = self.driver.window_handles[0]
+                    # print(main_window)
+
+                    actions = AC(self.driver)
+                    # actions.key_down(Keys.COMMAND).click(element).key_up(Keys.COMMAND).perform()
+                    actions.key_down(Keys.SHIFT)
+                    time.sleep(0.5)
+                    actions.click(element)
+                    actions.key_up(Keys.SHIFT)
+                    actions.perform()
+                    time.sleep(5)
+
+                    multiple_window = self.driver.window_handles
+                    for each_window in multiple_window[:0:-1]:
+                        self.driver.switch_to_window(each_window)
+                        time.sleep(5)
+                        new_url = self.driver.current_url
+
+                        try:
+                            exists_404 = driver.find_element_by_class_name('error404')
+                            page_is_404 = True
+                        except:
+                            page_is_404 = False
+
+                        self.driver.close()
+                        self.driver.switch_to_window(main_window)
+
+                        status = old_url != new_url and page_is_404 is False
+                        self.assertTrue(status, "Not Clickable or Page 404: {}".format(element.get_attribute('href')))
+
                 except:
-                    menu_elements.append("can't click on {}".format(items[i].text))
-        print(menu_elements)
-        self.assertIn('400', menu_elements, "Can't Click and open other tab in some element")
+                    print(element.text)
+
 
     def test_sub_menu_objects(self):
-        pass
+        """ Homepage - Sub Menu Elements, Verifying The Sub Elements/items with custom are complete or corret"""
+        items = self.main_page.get_main_menu_elements()
+        for i in range(0,len(items)):
+            if 'menu-item-object-servicio' in items[i].get_attribute('class'):
+                self.assertTrue(items[i].text in v.sub_elements, "The {} is not in the SubMenu/Service".format(items[i].text))
+
+            
 
     def test_sub_menu_objects_link(self):
         pass
@@ -91,12 +108,6 @@ class mainMenu(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.driver.close()
-
-    # def find_element(self, by, value):
-    #     try:
-    #         return self.driver.find_element(by, value)
-    #     except NoSuchElementException:
-    #         return None
 
 
 if __name__ == "__main__":
---
+++ variable.py
@@ -0,0 +1,38 @@
+elements = [
+'INICIO', 
+'PORTAFOLIO',
+'CONÓCENOS',
+'EMPLEOS',
+'BLOG']
+
+links = [
+    'http://gbh.com.do/',
+    'http://gbh.com.do/portafolio/',
+    'http://gbh.com.do/soluciones-gbh/',
+    'http://gbh.com.do/empleos/',
+    'http://gbh.com.do/noticias-de-tecnologia/']
+
+
+url = [
+"http://www.gbh.com.do",
+]
+
+html_class = [
+'menu-item-object-page',
+'menu-item-object-custom',
+'menu-item-object-servicio',
+'nav-servicios',
+]
+
+sub_elements = [
+'Asesoría de interfaz y experiencia de usuario (UI/UX)',
+'Consultoría Web'
+'Desarrollo de software'
+'SEO (optimización en motores de búsqueda)',
+'Diseño de páginas web',
+'Evaluación de infraestructura (IT)',
+'Certificación de cableado estructurado',
+'Instalación de cableado estructurado',
+'Instalación y configuración de redes Wi-Fi',
+'Configuración de respaldo',
+'Mantenimiento preventivo  y correctivo de computadoras en República Dominicana']
